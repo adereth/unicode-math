7745cb07fc572adf9d3226bb45c8eb0647d5d78e
Matt Adereth <adereth@gmail.com>
2013-09-23 18:56:06 -0400

    Adding URL and description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject unicode-math "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure library designed to let you painfully write easily readable math."
+  :url "https://github.com/Adereth/unicode-math"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
